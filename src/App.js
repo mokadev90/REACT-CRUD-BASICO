1f3a06e593b520e686640664dca16331876e1895
romeramatias <47081911+romeramatias2020@users.noreply.github.com>
2021-07-28 09:50:22 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CrudApp from "./components/CrudApp";
+
+function App() {
+  return (
+    <>
+      <h2>EJERCICIOS CON REACT</h2>
+      <CrudApp />
+      <hr />
+    </>
+  );
+}
+
+export default App;
