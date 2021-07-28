3539e8947b65a63d2af8bcad90324ff7136b6a5f
romeramatias <47081911+romeramatias2020@users.noreply.github.com>
2021-07-28 10:50:21 -0300

    incluyo json-server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import CrudApi from "./components/CrudApi";
 import CrudApp from "./components/CrudApp";
 
 function App() {
   return (
     <>
       <h2>EJERCICIOS CON REACT</h2>
+      <hr />
+      <CrudApi />
+      <hr />
       <CrudApp />
       <hr />
     </>
